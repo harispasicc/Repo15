8060a737ad44fa5f582429ae328064e13697fe27
Haris Pasic <harispasic@ymail.com>
2022-04-11 18:04:06 +0200

    Var and Let

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,1 +1,14 @@
+let myName;
+let message;
 
+myName = "Padawans";
+function myMsg(name) {
+    "use strict";
+    message = "Hello, " + myName + "!";
+
+    return message;
+}
+
+console.log(myMsg(message));
+
+module.exports = myMsg;
